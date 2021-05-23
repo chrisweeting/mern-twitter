f0aad723c37acc59efd5c3b922b15ce4a7e3da2a
chrisweeting <chrisweeting@gmail.com>
2021-05-23 19:37:51 -0400

    Prepare for heroku push

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const passport = require('passport');
 const express = require("express");
+const path = require('path');
 const app = express();
 // const db = require('./config/keys').mongoURI;
 const users = require("./routes/api/users");
@@ -37,3 +38,10 @@ app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
